31757eaa159a88a23a1ac0873ca533104fb8d206
Marc Brugger <github@bakito.ch>
2019-11-12 18:16:15 +0100

    Update upx.sh

--- .goreleaser/upx.sh
+++ .goreleaser/upx.sh
@@ -3,5 +3,5 @@
 #upx -q dist/${1}*/*
 
 # ignore darwin; see https://github.com/upx/upx/issues/222
-#find dist -type d -name "darwin*" -prune -o -type f -exec upx {} \;
+find dist -type d -name "darwin*" -prune -o -type f -exec upx {} \;
 
